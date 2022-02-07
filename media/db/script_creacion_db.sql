98860fcdedbc01c0bf6d436c9cc8eb13faf6774d
ran <ran-n@tutanota.com>
2022-02-07 22:42:21 +0100

    dates como str pq sqlite da noxo

--- media/db/script_creacion_db.sql
+++ media/db/script_creacion_db.sql
@@ -24,7 +24,7 @@ CREATE TABLE IF NOT EXISTS "paxina"(
 
 CREATE TABLE IF NOT EXISTS "top"(
     "id"        INTEGER UNIQUE NOT NULL,
-    "data"      DATE UNIQUE NOT NULL,
+    "data"      TEXT UNIQUE NOT NULL,
     "id_paxina" INTEGER NOT NULL,
     CONSTRAINT topPK PRIMARY KEY ("id"),
     CONSTRAINT divisaFK1 FOREIGN KEY ("id_paxina") REFERENCES "paxina"("id") ON UPDATE CASCADE MATCH [FULL]
@@ -33,7 +33,7 @@ CREATE TABLE IF NOT EXISTS "top"(
 CREATE TABLE IF NOT EXISTS "prezo"(
     "id_divisa"     TEXT NOT NULL,
     "id_divisa_ref" TEXT NOT NULL,
-    "data"          DATE UNIQUE NOT NULL,
+    "data"          TEXT UNIQUE NOT NULL,
     "valor"         FLOAT NOT NULL,
     CONSTRAINT prezoPK PRIMARY KEY ("id_divisa", "id_divisa_ref", "data"),
     CONSTRAINT prezoFK1 FOREIGN KEY ("id_divisa") REFERENCES "divisa"("id") ON UPDATE CASCADE MATCH [FULL],
@@ -44,7 +44,7 @@ CREATE TABLE IF NOT EXISTS "topx"(
     "id_divisa" TEXT NOT NULL,
     "id_top"    INTEGER NOT NULL,
     "posicion"  INTEGER NOT NULL,
-    "data"      DATE UNIQUE NOT NULL,
+    "data"      TEXT UNIQUE NOT NULL,
     CONSTRAINT prezoPK PRIMARY KEY ("id_divisa", "id_top"),
     CONSTRAINT prezoFK1 FOREIGN KEY ("id_divisa") REFERENCES "divisa"("id") ON UPDATE CASCADE MATCH [FULL],
     CONSTRAINT prezoFK1 FOREIGN KEY ("id_top") REFERENCES "top"("id") ON UPDATE CASCADE MATCH [FULL]
